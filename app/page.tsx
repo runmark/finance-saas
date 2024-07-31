e017825ac886d69249a7b1d2372b850f8160f432
maxingmin <ma_xingmin@163.com>
2024-07-31 14:37:32 +0000

    ✨ feat(clerk): clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       <Button>hello finance saas</Button>
     </div>
   );
