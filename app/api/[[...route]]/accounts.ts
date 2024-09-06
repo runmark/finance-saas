c4b98a2dc7dba5f4952f86875f63634739431a75
maxingmin <ma_xingmin@163.com>
2024-09-06 12:23:28 +0000

    ✨ feat(hono rpc): hono rpc

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,15 +1,28 @@
 import { accounts } from "@/db/schema";
 import { db } from "@/drizzle.config";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 
 const app = new Hono()
-    .get("/", async (c) => {
-        const data = await db.select({
-            id: accounts.id,
-            name: accounts.name,
-        }).from(accounts);
+    .get("/",
+        clerkMiddleware(),
+        async (c) => {
 
-        return c.json({ accounts: data });
-    });
+            const auth = getAuth(c);
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const data = await db
+                .select({
+                    id: accounts.id,
+                    name: accounts.name,
+                })
+                .from(accounts)
+                .where(eq(accounts.userId, auth.userId));
+
+            return c.json({ accounts: data });
+        });
 
 export default app;
