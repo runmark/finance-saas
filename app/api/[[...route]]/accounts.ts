ab7b7205258c642e31afaffd74965f71b6ff3156
maxingmin <ma_xingmin@163.com>
2024-09-20 13:56:52 +0000

    ✨ feat(schema): drizzle-zod schema

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,29 +1,50 @@
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 import { db } from "@/drizzle.config";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 
 const app = new Hono()
-    .get("/",
-        clerkMiddleware(),
-        async (c) => {
+  .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) {
+      // throw new HTTPException(401, {
+      //   res: c.json({ error: "Unauthorized" }, 401),
+      // });
+      return c.json({ error: "Unauthorized" }, 401);
+    }
 
-            const auth = getAuth(c);
-            if (!auth?.userId) {
-                // throw new HTTPException(401, "Unauthorized");
-                return c.json({ error: "Unauthorized" }, 401);
-            }
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
 
-            const data = await db
-                .select({
-                    id: accounts.id,
-                    name: accounts.name,
-                })
-                .from(accounts)
-                .where(eq(accounts.userId, auth.userId));
+    return c.json({ data });
+  })
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator("json", insertAccountSchema.pick({ name: true })),
+    async (c) => {
+      const auth = getAuth(c);
+      if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
 
-            return c.json({ data });
-        });
+      const values = c.req.valid("json");
+      const [data] = await db
+        .insert(accounts)
+        .values({
+          id: "test",
+          userId: auth.userId,
+          ...values,
+        })
+        .returning();
+
+      return c.json({ data });
+    }
+  );
 
 export default app;
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,26 +1,28 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import accounts from "./accounts";
 import authors from "./authors";
 import books from "./books";
-import accounts from "./accounts";
 
+// export const runtime = "edge";
 
-// export const runtime = 'edge'
-
-const app = new Hono().basePath('/api');
+const app = new Hono().basePath("/api");
 
 // app.use('*', clerkMiddleware());
 
 const routes = app
-    .route('/authors', authors)
-    .route('/books', books)
-    .route('/accounts', accounts);
+  .route("/authors", authors)
+  .route("/books", books)
+  .route("/accounts", accounts);
 
 // app.onError((err, c) => {
-//     return c.json({ error: err.message }, err.status);
+//   if (err instanceof HTTPException) {
+//     return err.getResponse();
+//   }
+//   return c.json({ error: "Internal Server Error" }, 500);
 // });
 
-export default app;
+// export default app;
 
 export const GET = handle(app);
 export const POST = handle(app);
